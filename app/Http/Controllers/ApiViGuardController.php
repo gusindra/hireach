d2fcd301c44304ed1e97b683a6e9bffcf5e2c0e9
gusindra <gusin44@gmail.com>
2024-05-14 09:10:41 +0800

    update middleware AP viGuard

--- app/Http/Controllers/ApiViGuardController.php
+++ app/Http/Controllers/ApiViGuardController.php
@@ -15,7 +15,134 @@ use App\Jobs\ProcessWaApi;
 
 class ApiViGuardController extends Controller
 {
+     /**
+     * test response
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function index(Request $request)
+    {
+        //return 1;
+        //get the request & validate parameters
+        $rules = [
+            'createDate' => 'required',
+            'uniqueTag' => 'required',
+            'monitoringDeviceName' => 'required',
+            'aiModelId' => 'required',
+            'aiModelName' => 'required',
+            'alarmDetails' => 'required',
+            'image' => 'required',
+        ];
+        /*$fields = $request->validate($rules);*/
+        $validator = \Validator::make($request->all(), $rules);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => $validator->messages(),
+                'code' => 400
+            ]);
+        }
+        //return 2;
+        try{
+            $customer = Client::where('tag', $request->uniqueTag)->first();
+            if($customer){
+                if($customer->source=='email'){
+                    $channel = 'email';
+                    $provider = 'provider1';
+                    $to = $customer->email;
+                    $from = 'alert@hireach.archeeshop.com';
+                }else{
+                    $channel = 'sms';
+                    $provider = 'provider1';
+                    $to = $customer->phone;
+                    $from = '081339668556';
+                }
+                $template = Template::where('trigger', $request->uniqueTag)->where('user_id', $customer->user_id)->first();
+                if($template){
+                    foreach($template->actions as $key => $action){
+                        // send request using template prt action
+                        $data[$key] = [
+                            'channel' => $channel,
+                            'provider' => $provider,
+                            'to' => $to,
+                            'from' => $from,
+                            'type' => 0,
+                            'title' => $request->alarmDetails,
+                            'text' => $this->convertText($request, $action->message),
+                            'templateid' => $template->id,
+                            'otp' => checkContentOtp($action->message)
+                        ];
+                        
+                        if($request->channel=='email'){
+                            $reqArr = json_encode($data);
+                            //THIS WILL QUEUE EMAIL JOB
+                            //ProcessEmailApi::dispatch($data, $customer->user, $reqArr);
+                        }elseif(strpos($request->channel, 'sms') !== false){
+                            //ProcessSmsApi::dispatch($data, $customer->user);
+                        }elseif($request->channel=='wa'){
+                            $credential = null;
+                            foreach($customer->user->credential as $cre){
+                                if($cre->client=='api_wa_mk'){
+                                    $credential = $cre;
+                                }
+                            }
+                            if($credential){
+                                //ProcessWaApi::dispatch($data, $credential);
+                            }else{
+                                return response()->json([
+                                    'message' => "Invalid credential",
+                                    'code' => 401
+                                ]);
+                            }
+                                
+                        }
+        
+                    }
+                    return response()->json([
+                        'message' => "Successful",
+                        'data' => $data,
+                        'code' => 0
+                    ]);
+                }else{
+                    return response()->json([
+                        'msg' => "Template Not Found",
+                        'code' => 400
+                    ]);
+                }
+            }elseif(!$customer){
+                return response()->json([
+                    'msg' => "Phone Number Not Found",
+                    'code' => 400
+                ]);
+            }else{
+                return response()->json([
+                    'msg' => "Team Chat Not Found",
+                    'code' => 400
+                ]);
+            }
+        }catch(\Exception $e){
+            return response()->json([
+                'msg' => "Invalid input",
+                'code' => 400
+            ]);
+        }
+        // show result on progress
+        // return response()->json([
+        //     'msg' => "Error! Nothing to save.",
+        //     'code' => 400
+        // ]);
+    }
+    
+    private function convertText($request, $action){
+        $text = $action;
+        $variable = [];
+        foreach($request->all() as $key => $req){
+            $variable[$key] = $req;
+        }
+        return bind_to_template($variable, $action);
+    }
+    
     /**
      * post new chat
      *
@@ -25,15 +152,24 @@ class ApiViGuardController extends Controller
     public function post(Request $request)
     {
         //get the request & validate parameters
-        $fields = $request->validate([
-            'createDate' => 'required|date',
+        $rules = [
+            'createDate' => 'required',
             'uniqueTag' => 'required',
             'monitoringDeviceName' => 'required',
             'aiModelId' => 'required',
             'aiModelName' => 'required',
             'alarmDetails' => 'required',
             'image' => 'required',
-        ]);
+        ];
+        /*$fields = $request->validate($rules);*/
+        $validator = \Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'msg' => $validator->messages(),
+                'code' => 400
+            ]);
+        }
 
         try{
             $customer = Client::where('tag', $request->uniqueTag)->first();
@@ -49,7 +185,7 @@ class ApiViGuardController extends Controller
                     $to = $customer->phone;
                     $from = '081339668556';
                 }
-                $template = Template::where('name', $request->alarmDetails)->where('user_id', $customer->user_id)->first();
+                $template = Template::where('trigger', $request->uniqueTag)->where('user_id', $customer->user_id)->first();
                 if($template){
                     foreach($template->actions as $key => $action){
                         // send request using template prt action
@@ -82,11 +218,10 @@ class ApiViGuardController extends Controller
                                 ProcessWaApi::dispatch($data, $credential);
                             }else{
                                 return response()->json([
-                                    'message' => "Invalid credential",
+                                    'msg' => "Invalid credential",
                                     'code' => 401
                                 ]);
                             }
-                                
                         }
         
                         return response()->json([
@@ -97,30 +232,30 @@ class ApiViGuardController extends Controller
                 }else{
                     return response()->json([
                         'msg' => "Template Not Found",
-                        'code' => 400
+                        'code' => 500
                     ]);
                 }
             }elseif(!$customer){
                 return response()->json([
                     'msg' => "Phone Number Not Found",
-                    'code' => 400
+                    'code' => 500
                 ]);
             }else{
                 return response()->json([
                     'msg' => "Team Chat Not Found",
-                    'code' => 400
+                    'code' => 500
                 ]);
             }
         }catch(\Exception $e){
             return response()->json([
                 'msg' => "Invalid input",
-                'code' => 400
+                'code' => 500
             ]);
         }
         // show result on progress
         return response()->json([
             'msg' => "Error! Nothing to save.",
-            'code' => 400
+            'code' => 500
         ]);
     }
 }
