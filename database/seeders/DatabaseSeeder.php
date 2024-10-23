699583aff64ab2c0aad82d9018cfc8b3c1372860
Ardana Yatra <imadeardanayatra@gmail.com>
2024-10-24 00:17:32 +0800

    🐞 fix: fix call seeder Template and action

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,7 +30,7 @@ class DatabaseSeeder extends Seeder
             SettingVatSeeder::class,
             SaldoUserSeeder::class,
             BillingUsersTableSeeder::class,
-            $this->call(ActionsAndTemplatesSeeder::class)
+          ActionsAndTemplatesSeeder::class
 
         ]);
     }
